3c7dd8d52caa24abd8f88ab55788fbb404025d6d
AxelMontlahuc <axel.montlahuc@proton.me>
2025-03-16 09:15:57 +0100

    Added a press_key(key) function + Infinite Square Path Loop test

--- controller.c
+++ controller.c
@@ -1,158 +1,75 @@
-#include <curl/curl.h>
-#include<windows.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <winsock2.h>
+#include <windows.h>
 
-void press_a() {
-  CURL *curl = curl_easy_init();
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5000/mgba-http/button/tap?key=A");
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);  // Enable POST
-    CURLcode res = curl_easy_perform(curl);
-    printf("Response: %d\n", res);
-    curl_easy_cleanup(curl);
-  }
-}
+#pragma comment(lib, "ws2_32.lib")
 
-void press_b() {
-  CURL *curl = curl_easy_init();
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5000/mgba-http/button/tap?key=B");
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);  // Enable POST
-    CURLcode res = curl_easy_perform(curl);
-    printf("Response: %d\n", res);
-    curl_easy_cleanup(curl);
+void press_key(char* input, SOCKET sock) {
+  char* message = malloc(128 * sizeof(char));
+  sprintf(message, "mgba-http.button.tap,%s", input);
+  printf("Message: %s\n", message);
+  char server_reply[1024];
+  int recv_size;
+  if (send(sock, message, strlen(message), 0) < 0) {
+    printf("Send failed. Error Code: %d\n", WSAGetLastError());
   }
-}
 
-void press_start() {
-  CURL *curl = curl_easy_init();
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5000/mgba-http/button/tap?key=Start");
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);  // Enable POST
-    CURLcode res = curl_easy_perform(curl);
-    printf("Response: %d\n", res);
-    curl_easy_cleanup(curl);
+  if ((recv_size = recv(sock, server_reply, sizeof(server_reply) - 1, 0)) == SOCKET_ERROR) {
+      printf("Receive failed. Error Code: %d\n", WSAGetLastError());
   }
-}
 
-void press_select() {
-  CURL *curl = curl_easy_init();
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5000/mgba-http/button/tap?key=Select");
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);  // Enable POST
-    CURLcode res = curl_easy_perform(curl);
-    printf("Response: %d\n", res);
-    curl_easy_cleanup(curl);
-  }
-}
-
-void press_up() {
-  CURL *curl = curl_easy_init();
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5000/mgba-http/button/tap?key=Up");
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);  // Enable POST
-    CURLcode res = curl_easy_perform(curl);
-    printf("Response: %d\n", res);
-    curl_easy_cleanup(curl);
-  }
-}
-
-void press_down() {
-  CURL *curl = curl_easy_init();
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5000/mgba-http/button/tap?key=Down");
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);  // Enable POST
-    CURLcode res = curl_easy_perform(curl);
-    printf("Response: %d\n", res);
-    curl_easy_cleanup(curl);
-  }
-}
-
-void press_left() {
-  CURL *curl = curl_easy_init();
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5000/mgba-http/button/tap?key=Left");
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);  // Enable POST
-    CURLcode res = curl_easy_perform(curl);
-    printf("Response: %d\n", res);
-    curl_easy_cleanup(curl);
-  }
-}
-
-void press_right() {
-  CURL *curl = curl_easy_init();
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5000/mgba-http/button/tap?key=Right");
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);  // Enable POST
-    CURLcode res = curl_easy_perform(curl);
-    printf("Response: %d\n", res);
-    curl_easy_cleanup(curl);
-  }
-}
-
-void press_l() {
-  CURL *curl = curl_easy_init();
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5000/mgba-http/button/tap?key=L");
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);  // Enable POST
-    CURLcode res = curl_easy_perform(curl);
-    printf("Response: %d\n", res);
-    curl_easy_cleanup(curl);
-  }
-}
-
-void press_r() {
-  CURL *curl = curl_easy_init();
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5000/mgba-http/button/tap?key=R");
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);  // Enable POST
-    CURLcode res = curl_easy_perform(curl);
-    printf("Response: %d\n", res);
-    curl_easy_cleanup(curl);
-  }
+  server_reply[recv_size] = '\0';
+  printf("Server reply: %s\n", server_reply);
+  Sleep(50);
 }
 
 int main() {
-  press_right();
-  Sleep(200);
-  press_right();
-  Sleep(200);
-  press_right();
-  Sleep(200);
-  press_right();
-  Sleep(200);
-  press_right();
-  Sleep(200);
-  press_up();
-  Sleep(200);
-  press_up();
-  Sleep(200);
-  press_up();
-  Sleep(200);
-  press_up();
-  Sleep(200);
-  press_up();
-  Sleep(200);
-  press_left();
-  Sleep(200);
-  press_left();
-  Sleep(200);
-  press_left();
-  Sleep(200);
-  press_left();
-  Sleep(200);
-  press_left();
-  Sleep(200);
-  press_down();
-  Sleep(200);
-  press_down();
-  Sleep(200);
-  press_down();
-  Sleep(200);
-  press_down();
-  Sleep(200);
-  press_down();
-  Sleep(200);
-  return 0;
+    WSADATA wsa;
+    SOCKET sock;
+    struct sockaddr_in server;
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("Winsock initialization failed. Error Code: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+        printf("Socket creation failed. Error Code: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(8888);
+
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        printf("Connection failed. Error Code: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    while (1) {
+      press_key("Right", sock);
+      press_key("Right", sock);
+      press_key("Right", sock);
+      press_key("Right", sock);
+      press_key("Up", sock);
+      press_key("Up", sock);
+      press_key("Up", sock);
+      press_key("Up", sock);
+      press_key("Left", sock);
+      press_key("Left", sock);
+      press_key("Left", sock);
+      press_key("Left", sock);
+      press_key("Down", sock);
+      press_key("Down", sock);
+      press_key("Down", sock);
+      press_key("Down", sock);
+    }
+
+    closesocket(sock);
+    WSACleanup();
+
+    return 0;
 }
