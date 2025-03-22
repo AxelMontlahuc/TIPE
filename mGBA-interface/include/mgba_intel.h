1981f806fb7750a6aa0702c4846977b863cdbc03
AxelMontlahuc <axel.montlahuc@proton.me>
2025-03-22 22:23:17 +0100

    Enhanced get_max_HP function. Now fully working

--- mGBA-interface/include/mgba_intel.h
+++ mGBA-interface/include/mgba_intel.h
@@ -6,6 +6,6 @@
 /**
  * @brief Gather the maximum HP of a pokemon from the game VRAM
  */
-int get_max_HP(SOCKET sock, char* address, int pokemon);
+int get_max_HP(SOCKET sock, int pokemon);
 
 #endif
--- mGBA-interface/src/mgba_intel.c
+++ mGBA-interface/src/mgba_intel.c
@@ -4,7 +4,30 @@
 #include <stdlib.h>
 #include <windows.h>
 
-int get_max_HP(SOCKET sock, char* address, int pokemon) {
+int get_max_HP(SOCKET sock, int pokemon) {
+    char* address;
+    switch (pokemon) {
+        case 1:
+            address = "0x02024544";
+            break;
+        case 2:
+            address = "0x020245A8";
+            break;
+        case 3:
+            address = "0x0202460C";
+            break;
+        case 4:
+            address = "0x02024670";
+            break;
+        case 5:
+            address = "0x020246D4";
+            break;
+        case 6:
+            address = "0x02024738";
+            break;
+        default:
+            return -1;
+    }
     char* message = malloc(128 * sizeof(char));
     sprintf(message, "memoryDomain.read16,wram,%s", address);
     char* server_reply = malloc(64 * sizeof(char));
@@ -15,9 +38,7 @@ int get_max_HP(SOCKET sock, char* address, int pokemon) {
     if ((recv_size = recv(sock, server_reply, 32 - 1, 0)) == SOCKET_ERROR) {
         printf("Receive failed. Error Code: %d\n", WSAGetLastError());
     }
-    printf("Server reply: %s\n", server_reply);
     server_reply[recv_size] = '\0';
     free(message);
-    printf("Server reply: %s\n", server_reply);
     return atoi(server_reply);
 }
