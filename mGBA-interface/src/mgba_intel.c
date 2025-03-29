32437d3932532241f6d700174dbad7fd942ef924
AxelMontlahuc <axel.montlahuc@proton.me>
2025-03-29 15:09:38 +0100

    Added a function to get party's pokemons DEF

--- mGBA-interface/src/mgba_intel.c
+++ mGBA-interface/src/mgba_intel.c
@@ -159,3 +159,42 @@ int get_ATK(SOCKET sock, int pokemon) {
     free(message);
     return atoi(server_reply);
 }
+
+int get_DEF(SOCKET sock, int pokemon) {
+    char* address;
+    switch (pokemon) {
+        case 1:
+            address = "0x02024548";
+            break;
+        case 2:
+            address = "0x020245AC";
+            break;
+        case 3:
+            address = "0x02024610";
+            break;
+        case 4:
+            address = "0x02024674";
+            break;
+        case 5:
+            address = "0x020246D8";
+            break;
+        case 6:
+            address = "0x0202473C";
+            break;
+        default:
+            return -1;
+    }
+    char* message = malloc(128 * sizeof(char));
+    sprintf(message, "memoryDomain.read8,wram,%s", address);
+    char* server_reply = malloc(64 * sizeof(char));
+    int recv_size;
+    if (send(sock, message, strlen(message), 0) < 0) {
+        printf("Send failed. Error Code: %d\n", WSAGetLastError());
+    }
+    if ((recv_size = recv(sock, server_reply, 32 - 1, 0)) == SOCKET_ERROR) {
+        printf("Receive failed. Error Code: %d\n", WSAGetLastError());
+    }
+    server_reply[recv_size] = '\0';
+    free(message);
+    return atoi(server_reply);
+}
