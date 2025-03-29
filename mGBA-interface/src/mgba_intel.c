d64a7a6ff58ea4ff5277698dd87652922025f69f
AxelMontlahuc <axel.montlahuc@proton.me>
2025-03-29 15:11:57 +0100

    Added a function to get party's pokemons ATK SPE

--- mGBA-interface/src/mgba_intel.c
+++ mGBA-interface/src/mgba_intel.c
@@ -237,3 +237,42 @@ int get_SPEED(SOCKET sock, int pokemon) {
     free(message);
     return atoi(server_reply);
 }
+
+int get_ATK_SPE(SOCKET sock, int pokemon) {
+    char* address;
+    switch (pokemon) {
+        case 1:
+            address = "0x0202454C";
+            break;
+        case 2:
+            address = "0x020245B0";
+            break;
+        case 3:
+            address = "0x02024614";
+            break;
+        case 4:
+            address = "0x02024678";
+            break;
+        case 5:
+            address = "0x020246DC";
+            break;
+        case 6:
+            address = "0x02024740";
+            break;
+        default:
+            return -1;
+    }
+    char* message = malloc(128 * sizeof(char));
+    sprintf(message, "memoryDomain.read8,wram,%s", address);
+    char* server_reply = malloc(64 * sizeof(char));
+    int recv_size;
+    if (send(sock, message, strlen(message), 0) < 0) {
+        printf("Send failed. Error Code: %d\n", WSAGetLastError());
+    }
+    if ((recv_size = recv(sock, server_reply, 32 - 1, 0)) == SOCKET_ERROR) {
+        printf("Receive failed. Error Code: %d\n", WSAGetLastError());
+    }
+    server_reply[recv_size] = '\0';
+    free(message);
+    return atoi(server_reply);
+}
